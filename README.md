e9ddc0e27afe5e7c348e9d21beb4a88ea012d76b
Ana Davis <anac.davis@gmail.com>
2019-11-04 01:41:57 -0700

    sql tables for product and  department added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bamazon
+
+
+
+
+# libraries
+1. MySql
+    1. https://www.npmjs.com/package/mysql#install
+2. Inquirer
+    1. https://www.npmjs.com/package/inquirer#installation
---
+++ schema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+name VARCHAR (50) NOT NULL,
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR (50) NOT NULL,
+price DOUBLE NOT NULL,
+stock_quantity INT NOT NULL,
+PRIMARY KEY (item_id)
+);
+
+CREATE TABLE department (
+department_id INT NOT NULL AUTO_INCREMENT,
+department_name VARCHAR (50) NOT NULL,
+PRIMARY KEY (department_id)
+
+
+)
