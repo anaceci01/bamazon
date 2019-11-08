ee023b9162f302359ba03e9e815c267cd506f9c3
Ana Davis <anac.davis@gmail.com>
2019-11-07 23:57:38 -0700

    dtabase created and tables working properly

--- schema.sql
+++ schema.sql
@@ -5,7 +5,6 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-name VARCHAR (50) NOT NULL,
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR (50) NOT NULL,
 price DOUBLE NOT NULL,
@@ -17,6 +16,6 @@ CREATE TABLE department (
 department_id INT NOT NULL AUTO_INCREMENT,
 department_name VARCHAR (50) NOT NULL,
 PRIMARY KEY (department_id)
-
-
 )
+
+
---
+++ seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (department_name, product_name, price, stock_quantity)
+VALUES ("household", "broom", 7.00, 50),
+("furniture", "bed", 200.00, 10),
+("electronic", "tv", 500.00, 40)
