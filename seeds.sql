fba4b9565a9e867fc7fa8f6edf295258f384d1ca
Ana Cecilia Davis <anaceci01@Anas-iMac.local>
2019-11-08 00:14:58 -0700

    household items added

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,16 @@
 INSERT INTO products (department_name, product_name, price, stock_quantity)
 VALUES ("household", "broom", 7.00, 50),
+("household", "Linens", 40.99, 20),
+("household", "Baking Dishes", 12.99, 15),
+("household", "towels", 14.99, 75),
+("household", "cofee filters", 2.99, 50),
+("furniture", "dresser", 780.00, 10),
+("furniture", "sofa", 1200.00, 5),
+("furniture", "chair", 90.00, 24),
 ("furniture", "bed", 200.00, 10),
-("electronic", "tv", 500.00, 40)
+("furniture", "desk", 700.00, 13),
+("electronic", "tv", 1000.00, 40)
+("electronic", "computer", 1200.00, 40)
+("electronic", "stero", 500.00, 19)
+("electronic", "bluetooth", 150.00, 24)
+("electronic", "phone", 750.00, 60)
