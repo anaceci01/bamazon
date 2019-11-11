44d27ea8c10d3eda1eb89abc60d2bccda0b23c3d
Ana Cecilia Davis <anaceci01@Anas-iMac.local>
2019-11-11 16:19:13 -0700

    saving any changes before dowloading software update to download mysql workbench

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+// MySql Connection and Inquirer Import
+var mysql = require ("mysql");
+var inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port:3306,
+    user:"root",
+    passord:
+})
