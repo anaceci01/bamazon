5c6fd1bae0eb1a80045ec24af64cbe7059534f9d
Ana Davis <anac.davis@gmail.com>
2019-11-12 23:13:01 -0700

    ught

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 var mysql = require("mysql");
-=======
-// MySql Connection and Inquirer Import
-var mysql = require ("mysql");
->>>>>>> 44d27ea8c10d3eda1eb89abc60d2bccda0b23c3d
 var inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
-<<<<<<< HEAD
     port: 3306,
     user: "root",
     password: "Sonora83",
@@ -38,9 +32,3 @@ function askUser() {
 
     })
 }
-=======
-    port:3306,
-    user:"root",
-    passord:
-})
->>>>>>> 44d27ea8c10d3eda1eb89abc60d2bccda0b23c3d
