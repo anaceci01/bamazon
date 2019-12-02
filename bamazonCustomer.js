3044efb0dc8aed9f9e0a93dc874c36af0a4981b1
Ana Davis <anac.davis@gmail.com>
2019-12-01 23:01:17 -0700

    promp for selection of product and function to add to cart functioning

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,7 @@ connection.connect(function(err) {
     if (err) throw err;
     custSelection();
 });
-
+// give customer a list of selections 
 function custSelection() {
     console.log('--------------------------------')
     inquirer
@@ -39,10 +39,40 @@ function custSelection() {
             }
         });
 }
-
+//products loaded after customer made selection
 function loadProducts() {
     connection.query("SELECT * FROM product", function(err, res) {
         if (err) throw err;
         console.table(res);
+    });
+}
+//function to add to cart
+function addToCart() {
+    connection.query("SELECT * FROM product", function(err, res) {
+        if (err) {
+            throw err;
+        }
+        inquirer.prompt([{
+                    type: "input",
+                    name: "item_id",
+                    message: "Select the Item ID you would like to buy"
+                },
+                {
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you would like to buy?"
+                }
+            ])
+            .then(function(answers) {
+                connection.query("SELECT * FROM product WHERE item_id = ?", [answers.item_id], function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    var amount = result[0].stock_quantity;
+                    if (answers.quantity > amount) {
+                        console.log("Sorry, we are out of stock!")
+                    }
+                })
+            });
     });
 }
